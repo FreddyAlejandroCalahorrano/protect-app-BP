0fb03189d5e0b9da7120952ea9b77724e26c9fbe
Freddy_Calahorrano <freddy.calahorrano01@epn.edu.ec>
2022-02-15 12:00:56 -0500

    cambios para realizar depply

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/proyecto-app-bp'));
+app.use(express.static('./dist/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/proyecto-app-bp/'}),
+    res.sendFile('index.html', {root: 'dist/index.html'}),
 );
 
 // Start the app by listening on the default Heroku port
